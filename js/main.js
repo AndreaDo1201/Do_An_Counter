7ac8d4d594391f16b37ebecc0fbc65d0c79241b7
AndreaDo1201 <an.dnn1201@gmail.com>
2026-03-20 02:17:54 -0400

    HTML updated
    In this commit, I updated html in order to animated the header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+<<<<<<< Updated upstream
+=======
+import { Counter }     from './counter.js';
+import { StepCounter } from './counter.js';
+
+
+const counter1 = new Counter("#counter-container1", 10, 5);
+const counter2 = new Counter("#counter-container2", 5);
+const counter3 = new Counter("#counterCon", 2);
+
+const step5  = new StepCounter("#step-counter1", 5, 5);
+const step10 = new StepCounter("#step-counter2", 15, 10);
+const step3  = new StepCounter("#step-counter3", 25, 3);
+>>>>>>> Stashed changes
