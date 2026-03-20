da7347b8bc1ca93b3712360d8a23dd43bbb59046
AndreaDo1201 <an.dnn1201@gmail.com>
2026-03-20 02:22:39 -0400

    Update text.js

--- js/text.js
+++ js/text.js
@@ -2,14 +2,14 @@ var tl = new TimelineMax({
   paused:true
 });
 
-tl.fromTo(".anim-typewriter", 8, {
+tl.fromTo(".type-writer", 8, {
   width: "0",
 }, {
   width: "20.18em", 
   ease:  SteppedEase.config(37)
 }, 0);
 
-tl.fromTo(".anim-typewriter", 0.5, {
+tl.fromTo(".type-writer", 0.5, {
   "border-right-color": "rgba(255,255,255,0.75)"
 }, {
   "border-right-color": "rgba(255,255,255,0)",
